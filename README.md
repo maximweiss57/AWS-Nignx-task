6e48c55e99312eebff254b6e25e098673285925a
Maxim weiss <137171905+maximweiss57@users.noreply.github.com>
2024-07-29 12:22:18 +0300

    updated terraform config, readme , added workflow directory

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
+Pre-requisites:
+Basic knowledge of Docker, Terraform, Linux and AWS.
+Terraform installed on your machine.
+configured the AWS CLI with the needed credentials.
+
 This repo contains:
-- A Dockerfile to build a Docker image based on the official Nginx image, with a custom index.html file.
+- A Dockerfile to build a Docker image based on the official Nginx image, with a custom index.html file that returnes "yo this is nignx".
 
 - Few terraform files to deploy the Docker container on AWS.
     - `main.tf` to create the needed infrastructure.
+        this file define the provider and terraform and creates the vpc &subnets
     - `sg.tf` to create the security group.
-    - `variables.tf` to initialize the variables.
+        2 security groups are created, 
+        one for the Bastion host, that will redirect the traffic to the main ec2 instance.
+        the other for the main ec2 instance, that will allow the traffic from the bastion host.
+    - `instances.tf` to create EC2 instances.
+        2 instances are created, the first ine is a bastion host, and the second one is the main instance that will run the nginx container.
+    - `variables.tf` to define the variables.
+        used to define the variables that will be used in the terraform files.
     - `vars.tfvars` to define the variables.
+        in this file you can change the variables as you see fit.
 
 - workflow files to automate the deployment process.
 
-
-    
+How this works:
+After hitting terrfaorm apply, the following will happen:
+- A VPC with 2 subnets will be created.
+- 2 security groups will be created
+- 2 EC2 instances will be created, one in each subnet.
+- The bastion host will redirect the traffic to the main instance.
+- The main instance will run the nginx container.
--- instances.tf
+++ instances.tf
@@ -1,8 +1,44 @@
+locals {
+  custom_instance_private_ip = aws_instance.nginx.private_ip
+}
+
 resource "aws_instance" "bastion" {
   ami                    = var.bastion-ami
   instance_type          = "t2.micro"
   vpc_security_group_ids = [module.allow_http_sg.security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y nginx
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+
+              # Configure NGINX
+              cat << 'EOF_NGINX' | sudo tee /etc/nginx/conf.d/default.conf
+              server {
+                  listen 80;
+
+                  # Define the server name (public IP of the EC2 instance in the public subnet)
+                  server_name ${aws_instance.bastion.public_ip};
+
+                  location / {
+                      # Forward (proxy) the request to the web server in the private subnet
+                      proxy_pass http://${local.custom_instance_private_ip};
+
+                      # Set headers to preserve the client's original information
+                      proxy_set_header Host \$host;
+                      proxy_set_header X-Real-IP \$remote_addr;
+                      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                      proxy_set_header X-Forwarded-Proto \$scheme;
+                  }
+              }
+              EOF_NGINX
+              
+              sudo systemctl restart nginx
+              EOF
+
   tags = {
     Name      = var.bastion-instance-name
     Terraform = "true"
@@ -10,10 +46,11 @@ resource "aws_instance" "bastion" {
 }
 
 resource "aws_instance" "nginx" {
-  ami                    = var.caustom-ami
+  ami                    = var.custom-ami
   instance_type          = "t2.micro"
   vpc_security_group_ids = [module.allow_bastion_sg.security_group_id]
   subnet_id              = module.vpc.private_subnets[0]
+
   tags = {
     Name      = var.custom-instance-name
     Terraform = "true"
