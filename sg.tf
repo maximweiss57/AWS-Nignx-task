d47f4c61c4931d879b83754da5168df1016e8fef
Maxim weiss <137171905+maximweiss57@users.noreply.github.com>
2024-07-29 20:33:14 +0300

    chore: Update .gitignore and add terraform.tfstate.backup

--- sg.tf
+++ sg.tf
@@ -43,3 +43,17 @@ module "allow_bastion_sg" {
     Terraform = "true"
   }
 }
+
+module "allow_ssh" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+
+  name        = "allow-ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["ssh-tcp"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+}
